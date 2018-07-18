fe0ca97b4071e34d841bfd1c519e12d04804e0bf
Jake Neild <jakeneildfb@yahoo.com>
2018-07-18 00:12:16 -0500

    First working script.

---
+++ README.txt
@@ -0,0 +1,3 @@
+This is a script that I use with Gekko to initialize 4 instances
+of gekko (one for each crypto I want to trade at this time), and ensure
+that each instance keeps running correctly, all in one terminal window.
---
+++ script.js
@@ -0,0 +1,69 @@
+require('shelljs/global');
+
+let status = {
+    btcClient: [],
+    bchClient: [],
+    ethClient: [],
+    ltcClient: []
+}
+
+let btcClient = function(){
+    exec("cd ../gekko && node gekko.js --config btcConfig.js", function(status, output) {
+        let reset = false
+        setInterval(function(){
+            if(status == 1 && reset == false){
+                console.log("--------------xxxxxxxxx1", status)
+                btcClient();
+                reset = true;
+            }
+        }, 60000);
+    });
+}
+
+let bchClient = function(){
+    exec("cd ../gekko && node gekko.js --config bchConfig.js", function(status, output) {
+        let reset = false;
+        setInterval(function(){
+            if(status == 1 && reset == false){
+                console.log("--------------xxxxxxxxx2", status)
+                bchClient();
+                reset = true;
+            }
+        }, 60000);
+    });
+}
+
+let ethClient = function(){
+    exec("cd ../gekko && node gekko.js --config ethConfig.js", function(status, output) {
+        let reset = false
+        setInterval(function(){
+            if(status == 1 && reset == false){
+                console.log("--------------xxxxxxxxx3", status)
+                ethClient();
+                reset = true;
+            }
+        }, 60000);
+    });
+}
+
+let ltcClient = function(){
+    exec("cd ../gekko && node gekko.js --config ltcConfig.js", function(status, output) {
+        let reset = false;
+        setInterval(function(){
+            if(status == 1 && reset == false){
+                console.log("--------------xxxxxxxxx4", status)
+                ltcClient();
+                reset = true;
+            }
+        }, 60000);
+    });
+}
+
+btcClient();
+bchClient();
+ethClient();
+ltcClient();
+
+
+
+//eth, ltc, btc, bch
